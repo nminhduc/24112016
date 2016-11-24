debfd02db808d65f845bc0d9569edeaa8a4a903d
hp <hp@DESKTOP-MR5UVRG>
2016-11-24 20:53:55 +0700

    Add project files.

---
+++ 24112016/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_24112016.MvcApplication" Language="C#" %>
---
+++ 24112016/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_24112016.Startup))]
+namespace _24112016
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
